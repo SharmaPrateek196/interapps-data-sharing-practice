ad066c2a77160b64123e8c9e42bb28affff8236d
SharmaPrateek196 <sharmaprateek196@gmail.com>
2022-02-23 12:40:27 +0530

    impl: sharing images

--- app/src/main/java/com/rangranjan/datasharingpractise/MainActivity.kt
+++ app/src/main/java/com/rangranjan/datasharingpractise/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.rangranjan.datasharingpractise
 
 import android.content.Intent
+import android.content.Intent.ACTION_SEND
+import android.content.Intent.EXTRA_STREAM
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -11,10 +16,28 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rangranjan.datasharingpractise.ui.theme.DataSharingPractiseTheme
 
 class MainActivity : ComponentActivity() {
-    val editTextString = mutableStateOf("")
+    private val editTextString = mutableStateOf("")
+
+    private val readWritePermissionContract = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissionsStatusMap ->
+        // if all permissions accepted
+        if (!permissionsStatusMap.containsValue(false)) {
+            takePictureIntent()
+        } else {
+            Toast.makeText(this, "all permissions not accepted", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val takePictureContract = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        uri?.let { safeUri ->
+            shareImageUriAsIntent(safeUri)
+        } ?: run {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,13 +58,34 @@ class MainActivity : ComponentActivity() {
                             },
                             Modifier.width(IntrinsicSize.Max)
                         )
-
+                        Box(modifier = Modifier.height(10.dp))
                         CustomButton(text = "Share text") {
                             startActivity(getTextActionSendIntent(editTextString.value))
+                        }
+                        Box(modifier = Modifier.height(50.dp))
+                        CustomButton(text = "Pick picture") {
+                            readWritePermissionContract.launch(arrayOf(
+                                android.Manifest.permission.READ_EXTERNAL_STORAGE
+//                                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                            ))
                         }
                     }
                 }
             }
+        }
+    }
+
+    private fun takePictureIntent() {
+            takePictureContract.launch("image/*")
+    }
+
+    private fun shareImageUriAsIntent(uri: Uri) {
+        Intent().apply {
+            action = ACTION_SEND
+            type = "image/*"
+            putExtra(EXTRA_STREAM, uri)
+        }.let { intent ->
+            startActivity(Intent.createChooser(intent, "Share Image via..."))
         }
     }
 }
@@ -65,9 +109,9 @@ fun CustomButton(text: String, onClick: () -> Unit) {
     }
 }
 
-fun getTextActionSendIntent(text: String): Intent {
+private fun getTextActionSendIntent(text: String): Intent {
     val intent = Intent().apply {
-        action = Intent.ACTION_SEND
+        action = ACTION_SEND
         putExtra(Intent.EXTRA_TEXT, text)
         type = "text/plain"
     }
